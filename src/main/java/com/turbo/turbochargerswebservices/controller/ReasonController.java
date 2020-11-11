e3fcfb82b7aab8c2074e979c9dd39aad5f80e8d3
m.krzyszczyk <marek.krzyszczyk@wp.pl>
2020-11-11 23:32:49 +0100

    added crud methods for reasons

--- src/main/java/com/turbo/turbochargerswebservices/controller/ReasonController.java
+++ src/main/java/com/turbo/turbochargerswebservices/controller/ReasonController.java
@@ -3,9 +3,9 @@ package com.turbo.turbochargerswebservices.controller;
 import com.turbo.turbochargerswebservices.model.Reason;
 import com.turbo.turbochargerswebservices.service.ReasonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,6 +24,30 @@ public class ReasonController {
     @GetMapping("/reasons")
     public List<Reason> listAllReasons() {
       return reasonService.getAllReasons();
+    }
+
+    @GetMapping("/reasons/{id}")
+    public ResponseEntity<Reason> getReasonById(@PathVariable Long id) {
+        Reason reason = reasonService.findReasonById(id);
+        return new ResponseEntity<>(reason, HttpStatus.OK);
+    }
+
+    @PutMapping("/reasons/{id}")
+    public ResponseEntity<Reason> updateReasonById(@RequestBody Reason reason) {
+        Reason updatedReason = reasonService.addReason(reason);
+        return new ResponseEntity<>(updatedReason, HttpStatus.OK);
+    }
+
+    @PostMapping("/reasons")
+    public ResponseEntity<Reason> createReason(@RequestBody Reason reason) {
+        Reason createdReason = reasonService.addReason(reason);
+        return new ResponseEntity<>(createdReason, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/reasons/{id}")
+    public ResponseEntity<Reason> deleteReason(@PathVariable Long id) {
+        reasonService.deleteReason(id);
+        return ResponseEntity.ok().build();
     }
 }
 
